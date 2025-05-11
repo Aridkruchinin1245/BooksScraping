c65ff392db9152e997d1c4d7959f18c415170163
Aridkruchinin1245 <misakrucinin80@gmail.com>
2025-05-11 20:40:49 +0300

    program can find all information now

--- main.py
+++ main.py
@@ -27,8 +27,32 @@ def get_price(currency_symbol) -> bool:
                 result.append(float(price[2:]))     
         print('working')    
     return result
-mas = []
-for price in get_price(currency_symbol=False):
-    if price < 30:
-        mas.append(price)
-print(mas)
+def get_data():
+    result = []
+    a = -1
+    page=49
+    hrefs = []
+    while True:
+        page+=1
+        url=f'https://books.toscrape.com/catalogue/page-{page}.html'
+        response = requests.get(url).text
+        soup = BeautifulSoup(response, 'lxml')
+        cards = soup.find_all('div', class_="image_container")
+        for card in cards:
+            hrefs.append('https://books.toscrape.com/catalogue/'+card.find('a')['href'])
+        if cards==[]:
+            print('pages are finish')
+            break
+        for href in hrefs:
+            a +=1
+            result.append([])
+            response = requests.get(href).text
+            card_data = BeautifulSoup(response, 'lxml')
+            name = card_data.find('h1').text
+            imgsrc = card_data.find('img')['src']
+            img = 'https://books.toscrape.com/'+imgsrc[5:]
+            price = float(card_data.find('p', class_='price_color').text[2:])
+            description = card_data.find('p', class_='')
+            result[a].extend([name,img,price,description])
+    return result
+get_data()
